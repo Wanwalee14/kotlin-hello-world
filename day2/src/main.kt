03c4ee358df6ddae4ed959373dc18474c2398226
Wanwalee14 <68175862+Wanwalee14@users.noreply.github.com>
2020-07-19 12:02:50 +0700

    newloop

--- day2/src/main.kt
+++ day2/src/main.kt
@@ -1,3 +1,10 @@
 fun main(){
-    println("this is day2!!!!!!!!")
+    var x = 1
+    println ("before the loop. x = $x.")
+    while (x < 4) {
+        println ("in the loop. x = $x.")
+        x = x + 1
+        println (" x is now $x.")
+    }
+    println ("after the loop. $x.")
 }
